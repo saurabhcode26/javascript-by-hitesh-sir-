8a4ace01d0e9992869acae5e98be4e6a7ad92aa9
saurabh maurya <125726759+saurabhcode26@users.noreply.github.com>
2023-12-21 03:03:57 +0000

    learnt about datatypes

---
+++ 01-js-basics/02-datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js files according to new js version.
+
+// alert("hi") //we are not using browser using node.js
+
+let name = "saurabh"//strings datatype
+let age = 23
+let isloggedin = true or false //boolean datatype
+let state = null;
+
+
+//primitive data types
+// number => 2 to power 53
+//bigint is also present to store big valure
+// string in ""
+// boolean => true/false
+// null => is a standalone value and also an object.
+// undefined=>  means value is not assigned
+//symbol => unique
+
+
+//object is also a datatype 
+
+
+//to check what variable have  datatype  we use (typeof datatypes)
