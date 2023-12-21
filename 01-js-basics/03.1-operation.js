55be47f643ee62f832900b9508000c5f1e2e4479
saurabh maurya <125726759+saurabhcode26@users.noreply.github.com>
2023-12-21 05:36:36 +0000

    studied about operation in js

---
+++ 01-js-basics/03.1-operation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negvalue = -value //ans is -3
+
+
+console.log(2+2); //we can perform any normal math.
+
+
+let str1 = "hello"
+let str2 ="saurabh"
+ 
+let str3 = str1 + str2;//output is hello saurabh
+
+console.log("1" + 2);//output 12
+console.log("1"+2+2);//output is 122
+console.log(1+2+"3");//output is 33
+
+//best to use () paranthesis in arthematic operation
+//if 1st value is string then all value will be converted in to string
+
+// prefer not to use
+console.log(+true)//output  is 1
+console.log(+"")//output is 0
+
+
+let gamecounter = 100;
+gamecounter++;//postfix operator
+++gamecounter;//prefix operator
+console.log(gamecounter);//output is 101
