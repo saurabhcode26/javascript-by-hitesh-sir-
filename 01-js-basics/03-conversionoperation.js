7dcbd6ab2e1e3374e37ceb81fe44e7d90c9d27d7
saurabh maurya <125726759+saurabhcode26@users.noreply.github.com>
2023-12-21 05:23:11 +0000

    conversion of data type

---
+++ 01-js-basics/03-conversionoperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueinnumber = Number(score)//gurantee that score is converted in number
+//null is converted in zero.
+//boolean is converted in 1(true),0(false)
+//"22abc" is converted in NaN
+
+let isloggedin =1 
+let boolean = Boolean(isloggedin)
+console.log(boolean);  //it will give output true
+//1 = true and 0 is false
+//"" false
+//"saurabh" is true
+
+let sumnumber = 33;
+let stringnumber =String(sumnumber)//number is converted in string
+
