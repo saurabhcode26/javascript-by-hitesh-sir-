e433273d798b5b781e1f74b45e346f8036ffc44d
saurabh maurya <125726759+saurabhcode26@users.noreply.github.com>
2023-12-21 06:17:54 +0000

    summary of datatypes in js

---
+++ 01-js-basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//Primitive data types 
+  // 7 types : String , Number , Bolean , null , undefined ,Symbol,bigint 
+//Reference data type (non primitive )
+   //array , object , function 
+
+//example of primitive data type
+const score = 100
+const scorevalue = 100.3
+const isloggedin = false
+const outsidetemp = null
+let userEmail;//undefined
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+//id and anotherid are two different values
+
+
+const bignumber = 3435534252435n; //js will use big int for big values
+
+
+//example of non privitive type
+const heros =["hulk","ironman"]
+let values {
+   name:saurabh,
+    age:22
+};
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof score);//output is number
+console.log(typeof outsidetemp);//output is object 
+
+
